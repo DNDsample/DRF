d0453e2b0c7d6b11559cfc79a58e3f2eb225805a
DNDsample <140153409+DNDsample@users.noreply.github.com>
2023-07-22 11:34:47 +0530

    Delete backend directory

--- backend/api/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-
-
-def api_home (request, *args,**kwargs):
-    body=request.body
-
-    print(body)
-    return JsonResponse( {"message":" hi there, this is your django api response"})
